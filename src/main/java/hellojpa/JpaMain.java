d7611fcf344d40ab179be9b661a30c966f39f7b4
liveforone <yc4852@gmail.com>
2022-03-06 16:45:39 +0900

    Good commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,10 +14,16 @@ public class JpaMain {
         tx.begin();  //트랜잭션 시작
 
         try {  //에러가 없을시
-            Member member = em.find(Member.class, 1L);  //조회
-            member.setName("HelloJPA");
+//            Member member = em.find(Member.class, 1L);  //조회
+//            member.setName("HelloJPA");  //수정
+////            em.persist(member);  //JPA에 저장
 
-//            em.persist(member);  //JPA에 저장
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();  //커밋해줌
         } catch (Exception e) {  //에러발생하면 롤백
