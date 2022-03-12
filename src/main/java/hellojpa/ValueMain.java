d5db05e6c7878c143d1e11b942510f814ac48b39
liveforone <yc4852@gmail.com>
2022-03-12 23:12:39 +0900

    Good commit

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -4,12 +4,13 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = a;
+       int a = 10;
+       int b = 10;
+       System.out.println("a == b : " + (a == b));
 
-        a = 20;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1 equals address2 :  " + (address1.equals(address2)));
     }
 }
