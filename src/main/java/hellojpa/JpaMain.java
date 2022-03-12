5847addca4c3b541aa522815551ccfdc64890ceb
liveforone <yc4852@gmail.com>
2022-03-12 11:40:26 +0900

    Good commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,20 +16,21 @@ public class JpaMain {
 
         try {  //에러가 없을시
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member1.getId());
-            System.out.println(findMember.getTeam().getName());
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();  //커밋해줌
         } catch (Exception e) {  //에러발생하면 롤백
